8189ed97d1bb4a5d84888087aa98e489bd1194e5
Paul Llanque <llanquepol@gmail.com>
2023-11-29 18:15:04 -0500

    [ADD] postgres datasource

---
+++ src/infrastructure/datasources/postgre-log.datasource.ts
@@ -0,0 +1,29 @@
+import { PrismaClient, SeveretuLevel } from '@prisma/client';
+import { LogDataSource } from '../../domain/datasources/log.datasource';
+import { LogEntity, LogSeveretyLevel } from '../../domain/entyties/log.entity';
+
+const severetyEnum = {
+  low: SeveretuLevel.LOW,
+  medium: SeveretuLevel.MEDIUM,
+  high: SeveretuLevel.HIGH,
+};
+
+const prismaClient = new PrismaClient();
+export class PostgresDataSource implements LogDataSource {
+  async savedLog(log: LogEntity): Promise<void> {
+    const level = severetyEnum[log.level];
+
+    await prismaClient.logModel.create({
+      data: {
+        ...log,
+        level,
+      },
+    });
+  }
+  async getLogs(severetyLever: LogSeveretyLevel): Promise<LogEntity[]> {
+    const level = severetyEnum[severetyLever];
+
+    const logs = await prismaClient.logModel.findMany({ where: { level } });
+    return logs.map(LogEntity.fromObject);
+  }
+}
