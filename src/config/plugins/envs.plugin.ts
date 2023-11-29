99bb36b20d10d6bc74289737c7b07ff1ce356b56
Paul Llanque <llanquepol@gmail.com>
2023-11-28 23:47:43 -0500

    [UPDATE] file envs.plugins

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -3,6 +3,7 @@ import * as env from 'env-var';
 
 export const envs = {
   PORT: env.get('PORT').required().asPortNumber(),
+  MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
   MAILER_MAIL: env.get('MAILER_MAIL').required().asEmailString(),
   MAILER_MAIL_SECRET_KEY: env
     .get('MAILER_MAIL_SECRET_KEY')
